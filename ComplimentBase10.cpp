941abe73b05da145cb09100a8b84b4c2c7260e77
iamkunal2k <kunalkumbhare8@gmail.com>
2023-01-26 18:39:28 +0530

    Compliment of base 10 is solved from leetcode

---
+++ ComplimentBase10.cpp
@@ -0,0 +1,34 @@
+// Compliment of base 10
+
+// https://leetcode.com/problems/complement-of-base-10-integer/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ int bitwiseComplement(int n) {
+        int m = n;
+        int mask = 0;
+
+        if (n==0) {
+            return 1;
+        }
+
+        while(m!=0) {
+            mask = (mask << 1 ) | 1;
+            m = m >> 1;
+
+        }
+
+        int ans = (~n) & mask ;
+        return ans;
+        
+    }
+
+
+int main() {
+	int n;
+	cin>>n;
+	cout<<bitwiseComplement(n)<<endl;
+}
