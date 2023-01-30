30d79726a641f153a055253e4581b09655b3b65c
iamkunal2k <kunalkumbhare8@gmail.com>
2023-01-30 22:35:06 +0530

    switch case calculator is added

---
+++ SwitchCase_Calc.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin>>a>>b;
+	char op;
+	cin>>op;
+
+	switch ( op ) {
+		case '+':
+		cout<<a+b<<endl;
+		break;
+
+		case '-':
+		cout<<a-b<<endl; 
+		break;
+
+		case '/':
+		cout<<a/b<<endl;
+		break;
+
+		case '*':
+		cout<<a*b<<endl;
+		break;
+	}
+}
