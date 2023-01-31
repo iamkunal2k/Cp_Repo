827b197a62ef6e08bcc377fc085bd876f07504a0
iamkunal2k <kunalkumbhare8@gmail.com>
2023-01-31 20:13:27 +0530

    PowerProblem with function is added

---
+++ PowerProblem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int a, int b) {
+	int ans = 1;
+	for (int i = 1; i < b; i++) {
+	ans = ans * a;
+	}
+	return ans;
+}
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+	cout<<power(a,b);
+}
