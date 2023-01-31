2be3dc52205866874e5135c820ca67b7f5e48ef8
iamkunal2k <kunalkumbhare8@gmail.com>
2023-01-31 21:03:40 +0530

    isPrime program is solved using functions

---
+++ PrintCounting.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int printCounting(int a) {
+	for(int i = 1; i<=a; i++) {
+		cout<<i<<" ";
+	}
+
+}
+
+int main() {
+	int a;
+	cin>>a;
+	cout<<printCounting(a);
+}
---
+++ isPrime.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n ) {
+   for(int i = 2; i < n; i++){
+       if(n % i == 0) {
+          return 0;
+        }
+   }
+   return 1;
+}
+
+int main() {
+   
+   int n ;
+   cin >> n;
+   if(isPrime(n)) {
+        cout <<" is a Prime number " <<endl;
+   }
+   else{
+       cout<< "Not a prime number "<<endl;
+
+   }
+}
