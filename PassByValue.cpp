cb4f124ef25001db92b3c206849f3fff40ebbdb0
iamkunal2k <kunalkumbhare8@gmail.com>
2023-02-02 00:01:35 +0530

    pass by value program using function is added

---
+++ PassByValue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dummy(int n ) {
+    n++;
+    cout << " n is " << n <<endl;
+
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    dummy(n);
+
+    cout<<"number n is "<< n << endl;
+
+
+    return 0;
+}
