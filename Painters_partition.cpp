b5cdf06a313719bdfe310c891854e56ef778e761
iamkunal2k <kunalkumbhare8@gmail.com>
2023-07-28 23:37:25 +0530

    Painters partition problem is solved using Binary Search  from codingNinjas and following similar logic as of allocate books problem

---
+++ Painters_partition.cpp
@@ -0,0 +1,55 @@
+// Problem Link :
+// https://www.codingninjas.com/studio/problems/painter-s-partition-problem_1089557?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries&leftPanelTab=1
+
+// Similar logic is followed from Allocate Books Problem
+
+
+bool possibleSoln(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+    
+    for(int i =0; i<n; i++) {
+        if(pageSum + arr[i] <= mid ) {
+            pageSum += arr[i];
+        }
+        else {
+            studentCount++;
+            if(studentCount> m || arr[i] > mid) {
+                return false;
+            }
+            pageSum = arr[i];
+        }
+    }
+    return true;
+}
+
+
+int allocateBooks(vector<int> arr, int n, int m) {
+    int s = 0;
+    int sum = 0;
+
+    for (int i =0 ; i < n; i++) {
+        sum += arr[i];
+    }
+    
+    int e = sum;
+    int mid = s +(e-s)/2;
+    int ans = -1;
+    
+    while(s <= e ) {
+        if(possibleSoln(arr, n, m, mid)) {
+            ans = mid;
+            e = mid-1;
+        }
+        else {
+            s = mid + 1 ;
+        }
+        mid = s +(e-s)/2;
+    }
+    return ans;
+}
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    //    Write your code here.
+    return allocateBooks(boards, boards.size(), k);
+}
