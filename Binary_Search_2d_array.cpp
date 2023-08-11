53b6d8d12f3ecc78a10446c4ef002550e58a44f4
iamkunal2k <kunalkumbhare8@gmail.com>
2023-08-11 22:57:56 +0530

    Search in 2d matrix problem is solved from codingNinjas which is similar to which we have solved from LeetCode

--- Binary_Search_2d_array.cpp
+++ Binary_Search_2d_array.cpp
@@ -1,5 +1,6 @@
 // Problem Link :
 // https://leetcode.com/problems/search-a-2d-matrix/description/
+// https://www.codingninjas.com/studio/problems/search-in-a-2d-matrix_980531?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
 
 class Solution {
 public:
@@ -36,3 +37,30 @@ public:
         
     }
 };
+
+
+
+/*
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int s = 0;
+        if(!matrix.size()) return false;
+        int e = (matrix.size() * matrix[0].size()) - 1;
+        
+        while(s <= e) {
+            int mid = (s + (e - s) / 2);
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] == target) {
+                return true;
+            }
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] < target) {
+                s = mid + 1;
+            }
+            else {
+                e = mid - 1;
+            }
+        }
+        return false;
+}
+
+
+*/
